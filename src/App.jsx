3b94350b2f4a642d2b3f14a9cc8ddcca06887d83
eleandro22 <eleandro.hoxha@gmail.com>
2024-12-11 21:58:57 +0100

    Do structure jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ import "./App.css";
 const App = () => {
   return (
     <div className="main-container">
-      <div className="sidebar"> Oklahoma City </div>
+      <div className="sidebar">
+        <input placeholder="search city"></input>
+        <h2 className="temperature-big"> 20 °C</h2>
+        <div className="details">
+          <h3 className="percentage">9.8% </h3>
+          <h3 className="wind-details"> Wind: WSW 6mph </h3>
+        </div>
+        <div className="description">
+          <h2 className="city-title"> Oklahoma City</h2>
+          <p>
+            The air quality is generally acceptable for most individuals.
+            However, sensitive groups may experience minor to moderate symptoms
+            from long-term exposure.
+          </p>
+        </div>
+      </div>
       <div className="content"> Content </div>
     </div>
   );
