4f32e2521ccfb92e85f59ce189637f063c6e520f
eleandro22 <eleandro.hoxha@gmail.com>
2024-12-16 14:41:00 +0100

    Complete the sidebar details

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import SearchForm from "../Sidebar/SearchForm/SearchForm";
 import CityDescription from ".//CityDescription/CityDescription";
+import Temperature from "./Temperature/Temperature";
 import WeatherDetails from "./WeatherDetails/WeatherDetails";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <SearchForm />
+      <Temperature />
       <WeatherDetails />
       <CityDescription />
     </div>
---
+++ src/components/Sidebar/Temperature/Temperature.jsx
@@ -0,0 +1,13 @@
+const Temperature = () => {
+  return (
+    <div className="temperature-container">
+      <h3>12°C</h3>
+      <div className="rough-details">
+        <p className="rain-percentage">9.8%</p>
+        <p className="wind-description">Wind at 6kmph</p>
+      </div>
+    </div>
+  );
+};
+
+export default Temperature;
